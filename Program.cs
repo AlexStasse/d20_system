a5b0dc621806b13291da319342736aa9d7dd2f93
alexstasse <turks.jpg@gmail.com>
2014-03-21 19:43:07 +1000

    First Commit
    c# is verbose as shit.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace console
+{
+	class Selector
+	{
+		//member variables
+		private int select;
+		enum Generator
+		{
+			Abilities, Race, Class, Skills, Feats, Description
+		}
+		public void Selection()
+		{
+			string[] generator = {"Abilities", "Race", "Class", "Skills", "Feats", "Description"};
+			select = Convert.ToInt16 (Console.ReadLine ());
+			Console.WriteLine ("Selected {0}", generator[select]);
+		}
+	}
+	class MainClass
+		{
+
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Welcome to Character Generator " +
+				"\nSelect what you would like to do next:" +
+				"\n\n1)Abilities\n2)Race\n3)Class\n4)Skills\n5)Feats\n6)Description");
+			Selector s = new Selector ();
+			s.Selection ();
+			Console.ReadLine ();
+		}
+	}
+}
