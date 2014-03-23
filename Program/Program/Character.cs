549afb5f22aebf3cdc31e172fe5d4a077d7bfa27
lepsa <owenlharvey@gmail.com>
2014-03-24 08:37:26 +1000

    Changed stat alters to be more sane.
    Rather than subtracting values I changed it to be addative. Makes using
    alters easier because += will increase stats and -= will reduce them
    now.

--- Program/Program/Character.cs
+++ Program/Program/Character.cs
@@ -8,258 +8,278 @@ using System.Runtime.CompilerServices;
 
 namespace Program
 {
-	public enum CharacterRace 
-	{
-		Elf, Dwarf, Gnome, HalfElf, Halfling, HalfOrc, Human
-	};
-	[SerializableAttribute]
-	public class Character
-	{
-		#region Character Stats
-		// Character stats. Using capitals because 'int' is a keyword.
-		private int STR;
-		private int DEX;
-		private int CON;
-		private int INT;
-		private int WIS;
-		private int CHA;
-		private int[] points = {-4, -2, -1, 0, 1, 2, 3, 5, 7, 10, 13, 17};
-		private int pointcost;
+    public enum CharacterRace
+    {
+        Elf,
+        Dwarf,
+        Gnome,
+        HalfElf,
+        Halfling,
+        HalfOrc,
+        Human}
 
-		// The get method calculates values then returns without the need for a function call.
-		public int strength
-		{
-			get
-			{
-				return this.STR - this.strAlter;
-			}
-			set
-			{
-				this.STR = value;
-			}
-		}
-		public int dexterity
-		{
-			get
-			{
-				return this.DEX - this.dexAlter;
-			}
-			set
-			{
-				this.DEX = value;
-			}
-		}
-		public int constitution
-		{
-			get
-			{
-				return this.CON - this.conAlter;
-			}
-			set
-			{
-				this.CON = value;
-			}
-		}
-		public int intelligence
-		{
-			get
-			{
-				return this.INT - this.intAlter;
-			}
-			set
-			{
-				this.INT = value;
-			}
-		}
-		public int wisdom
-		{
-			get
-			{
-				return this.WIS - this.wisAlter;
-			}
-			set
-			{
-				this.WIS = value;
-			}
-		}
-		public int charisma
-		{
-			get
-			{
-				return this.CHA - this.chaAlter;
-			}
-			set
-			{
-				this.CHA = value;
-			}
-		}
-		public int pointvalue
-		{
-			get
-			{
-				return this.pointcost = points [STR - 7] + points [DEX - 7] + points [CON - 7] + points [INT - 7] + points [WIS - 7] + points [CHA - 7];
-			}
-			set
-			{
-				this.pointcost = value;
-			}
-		}
+    ;
 
-		// Tempory stat changes
-		public int strAlter;
-		public int dexAlter;
-		public int conAlter;
-		public int intAlter;
-		public int wisAlter;
-		public int chaAlter;
+    [SerializableAttribute]
+    public class Character
+    {
+        #region Character Stats
 
-		// Stat modifier accessors.
-		public int strMod
-		{
-			get
-			{
-				return calcStatMod(this.strength);
-			}
-		}
-		public int dexMod
-		{
-			get
-			{
-				return calcStatMod(this.dexterity);
-			}
-		}
-		public int conMod
-		{
-			get 
-			{
-				return calcStatMod(this.constitution);
-			}
-		}
-		public int intMod
-		{
-			get
-			{
-				return calcStatMod(this.intelligence);
-			}
-		}
-		public int wisMod
-		{
-			get
-			{
-				return calcStatMod(this.wisdom);
-			}
-		}
-		public int chaMod
-		{
-			get
-			{
-				return calcStatMod(this.charisma);
-			}
-		}
-		#endregion
+        // Character stats. Using capitals because 'int' is a keyword.
+        private int STR;
+        private int DEX;
+        private int CON;
+        private int INT;
+        private int WIS;
+        private int CHA;
+        private int[] points = { -4, -2, -1, 0, 1, 2, 3, 5, 7, 10, 13, 17 };
+        private int pointcost;
+        // The get method calculates values then returns without the need for a function call.
+        public int strength
+        {
+            get
+            {
+                return this.STR + this.strAlter;
+            }
+            set
+            {
+                this.STR = value;
+            }
+        }
 
-		#region Character Race
-		public CharacterRace race;
-	
-		public void RaceStatMod ()
-		{
-			switch (race)
-			{
-			case CharacterRace.Dwarf:
-				conAlter -= 2;
-				wisAlter -= 2;
-				chaAlter += 2;
-				break;
-			case CharacterRace.Elf:
-				dexAlter -= 2;
-				intAlter -= 2;
-				conAlter += 2;
-				break;
-			case CharacterRace.Gnome:
-				conAlter -= 2;
-				chaAlter -= 2;
-				strAlter += 2;
-				break;
-			case CharacterRace.Halfling:
-				dexAlter -= 2;
-				chaAlter -= 2;
-				strAlter += 2;
-				break;
-			default:
-				break;
-			}
-		}
-		#endregion
+        public int dexterity
+        {
+            get
+            {
+                return this.DEX + this.dexAlter;
+            }
+            set
+            {
+                this.DEX = value;
+            }
+        }
 
-		#region Class Methods
-		// Class methods.
-		public Character()
-		{
-			// Stat defaulting.
-			int statDefault = 10;
-			this.strength = statDefault;
-			this.dexterity = statDefault;
-			this.constitution = statDefault;
-			this.intelligence = statDefault;
-			this.wisdom = statDefault;
-			this.charisma = statDefault;
+        public int constitution
+        {
+            get
+            {
+                return this.CON + this.conAlter;
+            }
+            set
+            {
+                this.CON = value;
+            }
+        }
 
-			// Ability Alter Defauling.
-			int alterDefault = 0;
-			this.strAlter = alterDefault;
-			this.dexAlter = alterDefault;
-			this.conAlter = alterDefault;
-			this.intAlter = alterDefault;
-			this.wisAlter = alterDefault;
-			this.chaAlter = alterDefault;
+        public int intelligence
+        {
+            get
+            {
+                return this.INT + this.intAlter;
+            }
+            set
+            {
+                this.INT = value;
+            }
+        }
 
-			// Default to human.
-			this.race = CharacterRace.Human;
-		}
+        public int wisdom
+        {
+            get
+            {
+                return this.WIS + this.wisAlter;
+            }
+            set
+            {
+                this.WIS = value;
+            }
+        }
 
-		private int calcStatMod(int stat)
-		{
-			return (stat / 2) - 5;
-		}
+        public int charisma
+        {
+            get
+            {
+                return this.CHA + this.chaAlter;
+            }
+            set
+            {
+                this.CHA = value;
+            }
+        }
 
+        public int pointvalue
+        {
+            get
+            {
+                return this.pointcost = points[STR - 7] + points[DEX - 7] + points[CON - 7] + points[INT - 7] + points[WIS - 7] + points[CHA - 7];
+            }
+            set
+            {
+                this.pointcost = value;
+            }
+        }
+        // Tempory stat changes
+        public int strAlter;
+        public int dexAlter;
+        public int conAlter;
+        public int intAlter;
+        public int wisAlter;
+        public int chaAlter;
+        // Stat modifier accessors.
+        public int strMod
+        {
+            get
+            {
+                return calcStatMod(this.strength);
+            }
+        }
 
-		// These methods should be eventually removed as they are not needed for the character data. They are more
-		// presentation and should be in something like the Editor class.
-		public void Race()
-		{
-			Console.Clear();
-			Console.WriteLine("Race");
-			Console.ReadLine();
-		}
+        public int dexMod
+        {
+            get
+            {
+                return calcStatMod(this.dexterity);
+            }
+        }
 
-		public void Class()
-		{
-			Console.Clear();
-			Console.WriteLine("Class");
-			Console.ReadLine();
-		}
+        public int conMod
+        {
+            get
+            {
+                return calcStatMod(this.constitution);
+            }
+        }
 
-		public void Skills()
-		{
-			Console.Clear();
-			Console.WriteLine("Skills");
-			Console.ReadLine();
-		}
+        public int intMod
+        {
+            get
+            {
+                return calcStatMod(this.intelligence);
+            }
+        }
 
-		public void Feats()
-		{
-			Console.Clear();
-			Console.WriteLine("Feats");
-			Console.ReadLine();
-		}
+        public int wisMod
+        {
+            get
+            {
+                return calcStatMod(this.wisdom);
+            }
+        }
 
-		public void Description()
-		{
-			Console.Clear();
-			Console.WriteLine("Description");
-			Console.ReadLine();
-		}
-		#endregion
-	}
+        public int chaMod
+        {
+            get
+            {
+                return calcStatMod(this.charisma);
+            }
+        }
+
+        #endregion
+
+        #region Character Race
+
+        public CharacterRace race;
+
+        public void RaceStatMod()
+        {
+            switch(race)
+            {
+                case CharacterRace.Dwarf:
+                    conAlter += 2;
+                    wisAlter += 2;
+                    chaAlter -= 2;
+                    break;
+                case CharacterRace.Elf:
+                    dexAlter += 2;
+                    intAlter += 2;
+                    conAlter -= 2;
+                    break;
+                case CharacterRace.Gnome:
+                    conAlter += 2;
+                    chaAlter += 2;
+                    strAlter -= 2;
+                    break;
+                case CharacterRace.Halfling:
+                    dexAlter += 2;
+                    chaAlter += 2;
+                    strAlter -= 2;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region Class Methods
+
+        // Class methods.
+        public Character()
+        {
+            // Stat defaulting.
+            int statDefault = 10;
+            this.strength = statDefault;
+            this.dexterity = statDefault;
+            this.constitution = statDefault;
+            this.intelligence = statDefault;
+            this.wisdom = statDefault;
+            this.charisma = statDefault;
+
+            // Ability Alter Defauling.
+            int alterDefault = 0;
+            this.strAlter = alterDefault;
+            this.dexAlter = alterDefault;
+            this.conAlter = alterDefault;
+            this.intAlter = alterDefault;
+            this.wisAlter = alterDefault;
+            this.chaAlter = alterDefault;
+
+            // Default to human.
+            this.race = CharacterRace.Human;
+        }
+
+        private int calcStatMod(int stat)
+        {
+            return (stat / 2) - 5;
+        }
+        // These methods should be eventually removed as they are not needed for the character data. They are more
+        // presentation and should be in something like the Editor class.
+        public void Race()
+        {
+            Console.Clear();
+            Console.WriteLine("Race");
+            Console.ReadLine();
+        }
+
+        public void Class()
+        {
+            Console.Clear();
+            Console.WriteLine("Class");
+            Console.ReadLine();
+        }
+
+        public void Skills()
+        {
+            Console.Clear();
+            Console.WriteLine("Skills");
+            Console.ReadLine();
+        }
+
+        public void Feats()
+        {
+            Console.Clear();
+            Console.WriteLine("Feats");
+            Console.ReadLine();
+        }
+
+        public void Description()
+        {
+            Console.Clear();
+            Console.WriteLine("Description");
+            Console.ReadLine();
+        }
+
+        #endregion
+    }
 }
 
--- Program/Program/Editor.cs
+++ Program/Program/Editor.cs
@@ -7,7 +7,7 @@ namespace Program
         public static void editor(Character character)
         {
             bool select = true;
-            while (select == true)
+            while(select == true)
             {
                 Console.Clear();
                 Console.WriteLine("Character Editor\nSelect what you would like to change\n\n" +
@@ -21,13 +21,13 @@ namespace Program
 
                 /*This is a series of menus which should call methods in the charEdit class to read/write variables and do calculations
                  * while all the menu functionality should be kept here*/
-                switch (Console.ReadLine().ToUpper())
+                switch(Console.ReadLine().ToUpper())
                 {
                     case "A":
                         Abilities(character);
                         break;
                     case "R":
-						Race(character);
+                        Race(character);
                         break;
                     case "C":
                         character.Class();
@@ -49,35 +49,34 @@ namespace Program
                 }
             }
         }
-
-		//-------------------------------------------------------------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------------------------------------------------------------
         public static void Abilities(Character character)
         {
             bool abchange = true;
-            while (abchange == true)
+            while(abchange == true)
             {
                 Console.Clear();
                 Console.WriteLine(
-					"Abilities currently cost {12} points.\n" +
-					"10: Low Fantasy\n" +
-					"15: Standard Fantasy\n" +
-					"20: High Fantasy\n" +
-					"25: Epic Fantasy\n\n" +
-					"  Ability:\t Score\t Modifier\n" +
-					"(S)trength:\t   {0} \t    {6}\n" +
-					"(D)exterity:\t   {1} \t    {7}\n" +
-					"(C)onstitution:\t   {2} \t    {8}\n" +
-					"(I)ntelligence:\t   {3} \t    {9}\n" +
-					"(W)isdom:\t   {4} \t    {10}\n" +
-					"Cha(r)isma:\t   {5} \t    {11}\n",
+                    "Abilities currently cost {12} points.\n" +
+                    "10: Low Fantasy\n" +
+                    "15: Standard Fantasy\n" +
+                    "20: High Fantasy\n" +
+                    "25: Epic Fantasy\n\n" +
+                    "  Ability:\t Score\t Modifier\n" +
+                    "(S)trength:\t   {0} \t    {6}\n" +
+                    "(D)exterity:\t   {1} \t    {7}\n" +
+                    "(C)onstitution:\t   {2} \t    {8}\n" +
+                    "(I)ntelligence:\t   {3} \t    {9}\n" +
+                    "(W)isdom:\t   {4} \t    {10}\n" +
+                    "Cha(r)isma:\t   {5} \t    {11}\n",
                     character.strength, character.dexterity, character.constitution, character.intelligence, character.wisdom, 
                     character.charisma, character.strMod, character.dexMod, character.conMod, character.intMod, character.wisMod,
-					character.chaMod, character.pointvalue);
+                    character.chaMod, character.pointvalue);
 
                 Console.WriteLine("Change which Ability? Or go(B)ack.");
-				string select = Console.ReadLine ().ToUpper ();
-				Console.WriteLine("New Value(7 to 18):");
-				switch (select)
+                string select = Console.ReadLine().ToUpper();
+                Console.WriteLine("New Value(7 to 18):");
+                switch(select)
                 {
                     case "S":
                         int newval = 0;
@@ -122,93 +121,95 @@ namespace Program
                 }
             }
         }
-		public static void Race(Character character)
-		{
-			bool racechange = true;
-			while (racechange == true)
-			{
-				Console.Clear ();
-                Console.WriteLine ("Choose a Race, currently selected is {0}\n\n" +
-				"1) Dwarf\t +2Con, +2 Wis, -2 Cha\n" +
-                "2) Elf\t\t +2 Dex, +2 Int, -2 Con\n" +
-				"3) Gnome\t +2 Con, +2 Cha, -2 Str\n" +
-				"4) Half Elf\t +2 to one ability score of your choice\n" +
-				"5) Halfling\t +2 Dex, +2 Cha, -2 Str\n" +
-				"6) Half Orc\t +2 to one ability score of your choice\n" +
-				"7) Human\t +2 to one ability score of your choice\n" +
-                    "B)ack", character.race);
 
-				switch (Console.ReadLine ().ToUpper ())
-				{
-				case "1":
-					character.race = CharacterRace.Dwarf;
-					character.RaceStatMod ();
-					break;
-				case "2":
-                    character.race = CharacterRace.Elf;
-					character.RaceStatMod ();
-					break;
-				case "3":
-                    character.race = CharacterRace.Gnome;
-					character.RaceStatMod ();
-					break;
-				case "4":
-					character.race = CharacterRace.HalfElf;
-					StatChoice (character);
-					break;
-				case "5":
-                    character.race = CharacterRace.Halfling;
-					character.RaceStatMod ();
-					break;
-				case "6":
-                    character.race = CharacterRace.HalfOrc;
-					StatChoice (character);
-					break;
-				case "7":
-                    character.race = CharacterRace.Human;
-					StatChoice (character);
-					break;
-				case "B":
-					racechange = false;
-					break;
-				}
-			}
-		}
-		public static void StatChoice(Character character)
-		{
-			Console.WriteLine ("Choose an Ability Score to increase by 2 points:\n" +
-			"(S)trength, (D)exterity, (C)onstitution, (I)ntelligence, (W)isdom, (C)harisma");
-			string select = Console.ReadLine ().ToUpper ();
-			bool statselect = true;
-			while (statselect == true)
-			{
-				statselect = false;
-			switch (select)
-			{
-			case "S":
-					character.strAlter -= 2;
-				break;
-			case "D":
-					character.dexAlter -= 2;
-				break;
-			case "C":
-					character.conAlter -= 2;
-				break;
-			case "I":
-					character.intAlter -= 2;
-				break;
-			case "W":
-					character.wisAlter -= 2;
-				break;
-			case "R":
-					character.chaAlter -= 2;
-				break;
-			default:
-				statselect = true;
-				break;
-				}
-			}
-		}
+        public static void Race(Character character)
+        {
+            bool racechange = true;
+            while(racechange == true)
+            {
+                Console.Clear();
+                Console.WriteLine("Choose a Race, currently selected is {0}\n\n" +
+                "1) Dwarf\t +2 Con, +2 Wis, -2 Cha\n" +
+                "2) Elf\t\t +2 Dex, +2 Int, -2 Con\n" +
+                "3) Gnome\t +2 Con, +2 Cha, -2 Str\n" +
+                "4) Half Elf\t +2 to one ability score of your choice\n" +
+                "5) Halfling\t +2 Dex, +2 Cha, -2 Str\n" +
+                "6) Half Orc\t +2 to one ability score of your choice\n" +
+                "7) Human\t +2 to one ability score of your choice\n" +
+                "B)ack", character.race);
+
+                switch(Console.ReadLine().ToUpper())
+                {
+                    case "1":
+                        character.race = CharacterRace.Dwarf;
+                        character.RaceStatMod();
+                        break;
+                    case "2":
+                        character.race = CharacterRace.Elf;
+                        character.RaceStatMod();
+                        break;
+                    case "3":
+                        character.race = CharacterRace.Gnome;
+                        character.RaceStatMod();
+                        break;
+                    case "4":
+                        character.race = CharacterRace.HalfElf;
+                        StatChoice(character);
+                        break;
+                    case "5":
+                        character.race = CharacterRace.Halfling;
+                        character.RaceStatMod();
+                        break;
+                    case "6":
+                        character.race = CharacterRace.HalfOrc;
+                        StatChoice(character);
+                        break;
+                    case "7":
+                        character.race = CharacterRace.Human;
+                        StatChoice(character);
+                        break;
+                    case "B":
+                        racechange = false;
+                        break;
+                }
+            }
+        }
+
+        public static void StatChoice(Character character)
+        {
+            Console.WriteLine("Choose an Ability Score to increase by 2 points:\n" +
+            "(S)trength, (D)exterity, (C)onstitution, (I)ntelligence, (W)isdom, (C)harisma");
+            string select = Console.ReadLine().ToUpper();
+            bool statselect = true;
+            while(statselect == true)
+            {
+                statselect = false;
+                switch(select)
+                {
+                    case "S":
+                        character.strAlter += 2;
+                        break;
+                    case "D":
+                        character.dexAlter += 2;
+                        break;
+                    case "C":
+                        character.conAlter += 2;
+                        break;
+                    case "I":
+                        character.intAlter += 2;
+                        break;
+                    case "W":
+                        character.wisAlter += 2;
+                        break;
+                    case "R":
+                        character.chaAlter += 2;
+                        break;
+                    default:
+                        statselect = true;
+                        break;
+                }
+            }
+        }
     }
 }
 
